901d66679a2efe8af0a68fecd19daa234b2f589e
Chirmaya <chirmayanashaar@gmail.com>
2016-06-24 23:40:33 -0500

    Missed files

---
+++ Assets/Scripts/Screen Controllers/BaseSaveOrLoadController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BaseSaveOrLoadController : ScriptableObject
+{
+    public GameDataController GameDataController { get; set; }
+    public Transform GameListContent { get; set; }
+    public GameObject GameListItem { get; set; }
+    public Dictionary<GameObject, GameData> GameDictionary { get; set; }
+
+    public void ReturnToPriorScreen()
+    {
+        GameDataController.GoToPreviousScreen();
+    }
+
+    public virtual void OnScreenAwake()
+    {
+        throw new Exception("OnScreenAwake not implemented");
+    }
+
+    public virtual string GetTitleText()
+    {
+        throw new Exception("GetTitleText not implemented");
+    }
+
+    public virtual void SaveOrLoad()
+    {
+        throw new Exception("SaveOrLoad not implemented");
+    }
+}
---
+++ Assets/Scripts/Screen Controllers/SaveLoadController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+public class SaveLoadController : ScreenController
+{
+    public Transform GameListContent;
+    public GameObject GameListItem;
+    public Text SaveOrLoadTitleText;
+    public Text SaveOrLoadButtonText;
+
+    private BaseSaveOrLoadController _saveOrLoadController;
+
+    internal override void OnScreenAwake()
+    {
+        Screen = _gameDataController.CurrentScreen;
+
+        SetupController();
+
+        SetupUI();
+
+        _saveOrLoadController.OnScreenAwake();
+    }
+
+    private void SetupController()
+    {
+        if (IsSaveGameScreen())
+        {
+            _saveOrLoadController = ScriptableObject.CreateInstance<SaveController>();
+        }
+        else
+        {
+            _saveOrLoadController = ScriptableObject.CreateInstance<LoadController>();
+        }
+
+        _saveOrLoadController.GameDataController = _gameDataController;
+        _saveOrLoadController.GameListContent = GameListContent;
+        _saveOrLoadController.GameListItem = GameListItem;
+    }
+
+    private bool IsSaveGameScreen()
+    {
+        if (Screen == Screen.Save)
+        {
+            return true;
+        }
+
+        if (Screen == Screen.Load)
+        {
+            return false;
+        }
+
+        throw new Exception("Current screen isn't save or load.");
+    }
+
+    private void SetupUI()
+    {
+        SaveOrLoadTitleText.text = _saveOrLoadController.GetTitleText();
+        SaveOrLoadButtonText.text = _saveOrLoadController.GetTitleText();
+    }
+
+    public void SaveOrLoad()
+    {
+        _saveOrLoadController.SaveOrLoad();
+    }
+
+}
